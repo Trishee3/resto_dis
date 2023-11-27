ef8c415e5a345e0004d271d13b27788993f070d0
Ervin Penales <theb8681@gmail.com>
2023-11-27 17:41:55 +0800

    fix db connection

--- Database.php
+++ Database.php
@@ -12,7 +12,7 @@
      		$this->conn = new mysqli($this->server_name, $this->db_username, $this->db_password, $this->db_name);
 
             if ($this->conn->connect_error){
-            	die("Usable to connect to Database" .$this->db_name . " : ". 4this->conn->connect_error);
+            	die("Usable to connect to Database" .$this->db_name . " : ". $this->conn->connect_error);
           }
      }    
 }    
