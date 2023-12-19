3056d307ad01c1733943b747117b205710c9bed7
Ervin Penales <theb8681@gmail.com>
2023-12-19 22:58:13 +0800

    update database

--- php-oop.sql
+++ php-oop.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:8889
--- Generation Time: Dec 18, 2023 at 10:20 AM
--- Server version: 5.7.34
--- PHP Version: 8.0.8
+-- Host: 127.0.0.1
+-- Generation Time: Dec 19, 2023 at 03:57 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -33,15 +33,15 @@ CREATE TABLE `products` (
   `product_name` varchar(100) NOT NULL,
   `price` float NOT NULL,
   `available` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `products`
 --
 
 INSERT INTO `products` (`id`, `image`, `product_name`, `price`, `available`) VALUES
-(11, '../assets/uploads/productImage/buffet.jpeg', 'Basic', 229, 38),
-(12, '../assets/uploads/productImage/test-upload.jpeg', 'MAX', 399, 21);
+(11, '../assets/uploads/productImage/buffet.jpeg', 'Basic', 229, 1),
+(12, '../assets/uploads/productImage/test-upload.jpeg', 'MAX', 399, 3);
 
 -- --------------------------------------------------------
 
@@ -55,9 +55,19 @@ CREATE TABLE `transactions` (
   `product_name` varchar(255) DEFAULT NULL,
   `quantity` int(11) DEFAULT NULL,
   `discount` float DEFAULT NULL,
+  `discount_type` varchar(255) DEFAULT NULL,
+  `num_of_discounted` int(11) DEFAULT NULL,
   `total_cost` float DEFAULT NULL,
   `transaction_Date` timestamp(6) NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `transactions`
+--
+
+INSERT INTO `transactions` (`id`, `customer_name`, `product_name`, `quantity`, `discount`, `discount_type`, `num_of_discounted`, `total_cost`, `transaction_Date`) VALUES
+(55, 'trisha test', 'Basic', 2, 0, 'None', 0, 458, '2023-12-19 14:49:24.000000'),
+(56, 'trish test 2', 'Basic', 2, 91.6, 'Person with Disability', 2, 366.4, '2023-12-19 14:49:46.000000');
 
 -- --------------------------------------------------------
 
@@ -69,14 +79,15 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `username`, `password`) VALUES
-(8, 'admin', '$2y$10$PWI.5694QsBuWdGMRZAoAuCUj6c5HSvIfOrL6ikweCD1zUpuKqsjm');
+(8, 'admin', '$2y$10$PWI.5694QsBuWdGMRZAoAuCUj6c5HSvIfOrL6ikweCD1zUpuKqsjm'),
+(9, 'trisha', '$2y$10$c3n4bNlv7nYjAxbUS1vSR.AzxqFtPAYBcpNxA5DOPR2D7JrBLwrOq');
 
 --
 -- Indexes for dumped tables
@@ -115,13 +126,13 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT for table `transactions`
 --
 ALTER TABLE `transactions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
